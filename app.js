05321febe2fd6cb151b96dd09986bf13b436d0d2
AntonEmery <antonemery@gmail.com>
2015-04-03 08:57:39 -0700

    basic bootstrap setup

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 //Kept getting a cross origin error with the code below and also Uncaught SyntaxError: Unexpected token :
 
-function go() {
-	$.ajax({ url: "http://api.civicapps.org/restaurant-inspections", type: "GET", 	dataType: "jsonp",
-	 success: (function(data) {
-			console.log('success');
-	 })
+// function go() {
+// 	$.ajax({ url: "http://api.civicapps.org/restaurant-inspections", type: "GET", 	dataType: "jsonp",
+// 	 success: (function(data) {
+// 			console.log('success');
+// 	 })
+// 	})
+// }
+
+
+$(document).ready(function() {
+	$('#overlay').show();
 	})
-}
 
 
 
