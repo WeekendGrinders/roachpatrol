fe79f319a12cf713efe3b69bcdc6b5de1c46d498
AntonEmery <antonemery@gmail.com>
2015-04-03 10:25:55 -0700

    basic jquery overlay

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-//Kept getting a cross origin error with the code below and also Uncaught SyntaxError: Unexpected token :
-
-// function go() {
-// 	$.ajax({ url: "http://api.civicapps.org/restaurant-inspections", type: "GET", 	dataType: "jsonp",
-// 	 success: (function(data) {
-// 			console.log('success');
-// 	 })
-// 	})
-// }
-
-
 $(document).ready(function() {
-	$('#overlay').show();
+	//$('#overlay').show();
 	})
+
+
+$(document).click(function(){
+	$('#overlay').hide();
+});
+	
 
 
 
