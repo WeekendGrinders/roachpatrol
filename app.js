cb84d52b0721f2ae686bf7b5e8af23b78968472c
AntonEmery <antonemery@gmail.com>
2015-04-04 22:15:36 -0700

    non static map loads

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-$(document).ready(function() {
-	//$('#overlay').show();
-	})
-
-
 $('button').click(function(){
 	$('#overlay').hide();
 });
 
 function getZip() {
 	var x = document.getElementById("zip").value;
-	$('.row').append('<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB_XSbaNGedhHV0UgkqMkV8tAA8FEuqi0k&q=' + x + '">' + '</iframe>');
-	}
+	console.log(x);
+	// $('.row').append('<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB_XSbaNGedhHV0UgkqMkV8tAA8FEuqi0k&q=' + x + '">' + '</iframe>');
+	 }
 
 	
 
