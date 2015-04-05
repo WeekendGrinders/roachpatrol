99b01c456e8db71469829b16913673702fee7272
AntonEmery <antonemery@gmail.com>
2015-04-04 20:47:20 -0700

    map loads off zip input

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ $('button').click(function(){
 
 function getZip() {
 	var x = document.getElementById("zip").value;
-	$('.row').append('<h2>' + x  + '</h2>');
+	$('.row').append('<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB_XSbaNGedhHV0UgkqMkV8tAA8FEuqi0k&q=' + x + '">' + '</iframe>');
 	}
 
 	
