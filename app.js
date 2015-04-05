49a21c220c75ca5849795a352dc1b05b42dd90fb
AntonEmery <antonemery@gmail.com>
2015-04-04 22:34:32 -0700

    non static map embeds, overlay hide works again

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+$(document).ready(function() {
+	//$('#overlay').show();
+	})
+
+
 $('button').click(function(){
 	$('#overlay').hide();
 });
@@ -5,7 +10,6 @@ $('button').click(function(){
 function getZip() {
 	var x = document.getElementById("zip").value;
 	console.log(x);
-	// $('.row').append('<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB_XSbaNGedhHV0UgkqMkV8tAA8FEuqi0k&q=' + x + '">' + '</iframe>');
 	 }
 
 	
