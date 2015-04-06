7da926641091a898185def5321060027c8f675a4
AntonEmery <antonemery@gmail.com>
2015-04-05 20:59:58 -0700

    loads zip code from form submit

--- app.js
+++ app.js
@@ -9,8 +9,37 @@ $('button').click(function(){
 
 function getZip() {
 	var x = document.getElementById("zip").value;
-	console.log(x);
-	 }
+	google.maps.event.addDomListener(window, 'load', initialize);
+}
+	 
+
+
+    var geocoder;
+
+      function initialize() {
+      	geocoder = new google.maps.Geocoder();
+      	var mapOptions = {
+          zoom: 14
+        };
+    		geocoder.geocode({
+    		address: document.getElementById("zip").value
+    		}, function(results, status){
+    			if(status == google.maps.GeocoderStatus.OK) {
+    				map.setCenter(results[0].geometry.location);
+    				new google.maps.Marker({
+    					map: map,
+    					position: results[0].geometry.location	
+    					})
+    				} else {
+    					alert('Geocode was not successful');
+    					}
+    	})
+        
+       
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+      }
+    
 
 	
 
