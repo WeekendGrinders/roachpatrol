259d5b6b2a9144bec60598fc2c6c534fa30defda
AntonEmery <antonemery@gmail.com>
2015-04-06 19:18:44 -0700

    multiple points posting on map

--- app.js
+++ app.js
@@ -34,23 +34,19 @@ function initialize() {
 				}
 
 			for (i = 0; i < locations.length; i++) {
-    marker = new google.maps.Marker({
-    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-    map: map
-  	});
-  }
+		    marker = new google.maps.Marker({
+		    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+		    map: map
+		  	});
+  		}
 
-   google.maps.event.addListener(marker, 'click', (function (marker, i) {
+   		google.maps.event.addListener(marker, 'click', (function (marker, i) {
         return function () {
             infowindow.setContent(locations[i][0]);
             infowindow.open(map, marker);
-        }
-    })(marker, i));
+        		}
+    			})(marker, i));
 			})
-
-
-		
-
 
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
@@ -60,7 +56,9 @@ function initialize() {
 /* test hard coded lat and long */
 
 var locations = [
-        ['Portland',  45.5236111, -122.675, 4]
+        ['Portland',  45.5236111, -122.675, 4],
+        ['Pittok Mansion', 45.5250, -122.7164],
+        ['Council Crest', 45.5001, -122.7093]
     ];
     
 
