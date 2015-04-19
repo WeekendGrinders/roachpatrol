a46a21885f54b25c5c39f0e118815e605b36986f
mtupper <michael.tupper@gmail.com>
2015-04-19 16:55:00 -0700

    fixing oversight on models

--- app/js/models.js
+++ app/js/models.js
@@ -12,7 +12,7 @@ app.Restaurant = Backbone.Model.extend({
 app.restaurant = new app.Restaurant({});
 
 app.RestaurantList = Backbone.Collection.extend({
-    url: '/go',
+    //url: '/go',
     model: app.Restaurant,
     initialize: function() {
         this.on('remove', this.hideModel);
@@ -23,17 +23,14 @@ app.RestaurantList = Backbone.Collection.extend({
 });
 
 app.restaurantList = new app.RestaurantList({});
-app.restaurantList.fetch();
+//app.restaurantList.fetch();
 
-var testSet = {
-    result: 'OK',
-    arr: [
+var testSet = [
     {name:'Dive 1', score:75},
     {name:'Dive 2', score:80},
     {name:'Dive 3', score:85},
     {name:'Dive 4', score:90},
     {name:'Dive 5', score:95}
-    ]
-};
+    ];
 
-app.restaurantList.reset(testSet.arr);
+app.restaurantList.reset(testSet);
