da78b51ec1a3d26d5cd281501874469420037a1f
KyleHouston84 <chariot124@gmail.com>
2015-04-20 19:19:26 -0700

    cleaning house a bit...

--- capstoneNODE.js
+++ capstoneNODE.js
@@ -26,42 +26,42 @@ function api(response, query) {
 	http.get("http://api.civicapps.org/restaurant-inspections/near/" + lngLat + "?distance=2&count=20000", function (res) {
 		console.log("Got response: " + res.statusCode);
 		res.on('data', function (chunk) {
-		   	body += chunk;
-		   	console.log("---------------Recieved a chunk of data from API--------------");
+			body += chunk;
+			console.log("---------------Recieved a chunk of data from API--------------");
 		});
 		res.on('end', function () {
 			console.log(body);
-		   	var obj = JSON.parse(body);
-		   	console.log("---------------closing connection with server--------------");
-		   	//removing inspections with the score of 0 and combining inspection ids in to an array for multipule inspection on the same restaurant
-		   	for (var i = (obj.results.length - 1); i > -1; i--) {
-		   		//Changing inspection_number to an array
-		   		obj.results[i].inspection_number = [obj.results[i].inspection_number];
+			var obj = JSON.parse(body);
+			console.log("---------------closing connection with server--------------");
+			//removing inspections with the score of 0 and combining inspection ids in to an array for multipule inspection on the same restaurant
+			for (var i = (obj.results.length - 1); i > -1; i--) {
+				//Changing inspection_number to an array
+				obj.results[i].inspection_number = [obj.results[i].inspection_number];
 
-		   		if (obj.results[i].score != 0) {
-		   			//This record has a score over 0
-		   			var index = -1;
-		   			for(var j = 0; j < tempArr.length; j++) {
-		   				//searching tempArr for a record with the same id
-					    if (tempArr[j].id === obj.results[i].id) {
-					        //found it, pass along the index of the record
-					        index = j;
-					        break;
-					    }
+				if (obj.results[i].score != 0) {
+					//This record has a score over 0
+					var index = -1;
+					for(var j = 0; j < tempArr.length; j++) {
+						//searching tempArr for a record with the same id
+						if (tempArr[j].id === obj.results[i].id) {
+							//found it, pass along the index of the record
+							index = j;
+							break;
+						}
 					};
 					if (index >= 0){
 						tempArr[index].inspection_number.push(obj.results[i].inspection_number) 
 					} else {
-		   			//This restaurant is not in tempArr yet
-		   			tempArr.push(obj.results[i])
-		   			};
-		   		};
-		   	}
-		   	obj.results = tempArr;
-		   	response.end(JSON.stringify(obj));
+					//This restaurant is not in tempArr yet
+					tempArr.push(obj.results[i])
+					};
+				};
+			}
+			obj.results = tempArr;
+			response.end(JSON.stringify(obj));
 		});
 		res.on('error', function (e) {
-		   	console.log("Got error: " + e.message);
+			console.log("Got error: " + e.message);
 		});
 	});
 	console.log("---------------End of API function--------------");
@@ -88,25 +88,25 @@ function apiBackbone(response, query) {
 	http.get("http://api.civicapps.org/restaurant-inspections/near/" + lngLat + "?distance=2&count=20000", function (res) {
 		console.log("Got response: " + res.statusCode);
 		res.on('data', function (chunk) {
-		   	body += chunk;
-		   	console.log("---------------Recieved a chunk of data from API--------------");
+		body += chunk;
+		console.log("---------------Recieved a chunk of data from API--------------");
 		});
 		res.on('end', function () {
 			//console.log(body);
-		   	var obj = JSON.parse(body);
-		   	console.log("---------------closing connection with server--------------");
-		   	//removing inspections with the score of 0
-		   	for (var i = (obj.results.length - 1); i > -1; i--) {
-		   		if (obj.results[i].score == 0) {
-		   		} else {
-		   			arrBackbone.push(obj.results[i]);
-		   		};
-		   	}
-		   	console.log("Array: " + arrBackbone);
-		   	//response.end(arrBackbone);
+			var obj = JSON.parse(body);
+			console.log("---------------closing connection with server--------------");
+			//removing inspections with the score of 0
+			for (var i = (obj.results.length - 1); i > -1; i--) {
+				if (obj.results[i].score == 0) {
+				} else {
+					arrBackbone.push(obj.results[i]);
+				};
+			}
+			console.log("Array: " + arrBackbone);
+			response.end(JSON.stringify(arrBackbone));
 		});
 		res.on('error', function (e) {
-		   	console.log("Got error: " + e.message);
+			console.log("Got error: " + e.message);
 		});
 	});
 	console.log("---------------End of API function--------------");
@@ -119,17 +119,17 @@ function getReport(response, query) {
 	http.get("http://api.civicapps.org/restaurant-inspections/inspection/" + query, function (res) {
 		console.log("Got response: " + res.statusCode);
 		res.on('data', function (chunk) {
-		   	body2 += chunk;
-		   	console.log("---------------Recieved a chunk of data from API--------------");
+			body2 += chunk;
+			console.log("---------------Recieved a chunk of data from API--------------");
 		});
 		res.on('end', function () {
 			console.log(body2);
-		   	var obj = JSON.parse(body2);
-		   	console.log("---------------closing connection with server--------------");
-		   	response.end(body2);
+			var obj = JSON.parse(body2);
+			console.log("---------------closing connection with server--------------");
+			response.end(body2);
 		});
 		res.on('error', function (e) {
-		   	console.log("Got error: " + e.message);
+			console.log("Got error: " + e.message);
 		});
 	});
 	console.log("---------------End of API function--------------");
